f2ca1f0d8b3d5da080a1bd1a36fd120a2e72cd4e
vedant <vedant@ssipmt.com>
2024-01-29 06:51:42 +0530

    costom hooks

--- src/component/PokemonList/PokemonList.jsx
+++ src/component/PokemonList/PokemonList.jsx
@@ -1,56 +1,10 @@
 
-import { useEffect, useState } from "react";
-import axios from 'axios';
 import Pokemon from "../Pokemon/Pokemon.jsx";
+import usePokemonList from "../../Hooks/usePokemonList.js";
 
 function PokemonList() {
 
-    const [pokemonListState,setpokemonListState]=useState({
-        pokemonList:[],
-        isLoading: true,
-        pokedex_url:'https://pokeapi.co/api/v2/pokemon',
-        nexturl:'',
-        prevurl:'',
-
-    })
-    
-    async function downloadPokemons() {
-
-        setpokemonListState((state)=>({...state ,isLoading:true}));
-
-        const response = await axios.get(pokemonListState.pokedex_url);
-        const pokemonResult = response.data.results;
-        console.log(pokemonResult);
-
-        setpokemonListState((state)=> ({
-            ...state,
-            nexturl:response.data.next,
-            prevurl:response.data.previous
-        }));
-
-        const pokemonResultPromise = pokemonResult.map((pokemon) => axios.get(pokemon.url));
-
-        const pokemonData = await axios.all(pokemonResultPromise);
-
-        console.log(pokemonData);
-        
-        const PokeListres = pokemonData.map((pokedata) => {
-            const pokemon = pokedata.data;
-            return {
-                id: pokemon.id,
-                name: pokemon.name,
-                image: pokemon.sprites.other.dream_world.front_default,
-                types: pokemon.types
-            }
-        })
-        console.log(PokeListres);
-        setpokemonListState((state)=> ({...state, pokemonList:PokeListres , isLoading:false}));
-    }
-
-    useEffect(() => {
-        downloadPokemons();
-      
-    },[pokemonListState.pokedex_url]);
+    const { pokemonListState,setpokemonListState }=usePokemonList();
 
  
     return (
